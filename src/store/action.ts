154686264416e3803f1464da32953727bb81b1f9
MaxGladee <mrlokomax@gmail.com>
2026-05-04 02:05:28 +0500

    Добро пожаловать, или посторонним вход воспрещён (часть 1)

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,6 +2,9 @@ import { createAction } from '@reduxjs/toolkit';
 import { AxiosInstance } from 'axios';
 import { AppDispatch, RootState } from './index';
 import { Offer } from '../types/offer';
+import { User } from '../types/user';
+import { AuthorizationStatus } from '../const';
+import { TOKEN_KEY } from '../services/api';
 
 export const changeCity = createAction<string>('app/changeCity');
 
@@ -9,6 +12,9 @@ export const loadOffers = createAction<Offer[]>('app/loadOffers');
 
 export const setOffersDataLoading = createAction<boolean>('app/setOffersDataLoading');
 export const setOffersDataError = createAction<string | null>('app/setOffersDataError');
+
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
+export const setUser = createAction<User | null>('user/setUser');
 
 export const fetchOffersAction = () =>
   async (dispatch: AppDispatch, _getState: () => RootState, api: AxiosInstance) => {
@@ -26,3 +32,30 @@ export const fetchOffersAction = () =>
       dispatch(setOffersDataLoading(false));
     }
   };
+
+export const checkAuthAction = () =>
+  async (dispatch: AppDispatch, _getState: () => RootState, api: AxiosInstance) => {
+    try {
+      const { data } = await api.get<User>('/login');
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+      dispatch(setUser(data));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+      dispatch(setUser(null));
+    }
+  };
+
+export const loginAction = (email: string, password: string) =>
+  async (dispatch: AppDispatch, _getState: () => RootState, api: AxiosInstance) => {
+    try {
+      const { data } = await api.post<User>('/login', { email, password });
+      localStorage.setItem(TOKEN_KEY, data.token);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+      dispatch(setUser(data));
+    } catch (error) {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+      throw error;
+    }
+  };
+
+export const logout = createAction('user/logout');
